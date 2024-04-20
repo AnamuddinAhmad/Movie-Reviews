c0c5e6c141cc1beee842ad70984e015ba1add0af
Anamuddin Ahmad <119073478+AnamuddinAhmad@users.noreply.github.com>
2024-04-21 01:24:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let movieNameRef = document.getElementById("movie-name");
+let searchBtn = document.getElementById("search-btn");
+let result = document.getElementById("result");
+
+let getMovie = () => {
+  let movieName = movieNameRef.value;
+  let url = `https://www.omdbapi.com/?t=${movieName}&apikey=5f4d31e2`;
+  //if input field is empty
+  if (movieName.length <= 0) {
+    result.innerHTML = `<h3 class="msg"> Please Enter A Movie Name</h3>`;
+  } else {
+    fetch(url)
+      .then((resp) => resp.json())
+      .then((data) => {
+        if(data.Response == 'True'){
+          result.innerHTML = `<div class="info">
+          <img src=${data.Poster} class="poster">
+          <div>
+          <h2>${data.Title}</h2>
+          <div class="rating">
+          <img src="./icons8-star-filled-48.png" alt="">
+          <h4>${data.imdbRating}</h4>
+          </div>
+          <div class="details">
+          <span>${data.Rated}</span>
+          <span>${data.Year}</span>
+          <span>${data.Runtime}</span>
+          </div>
+          <div class="genre">
+          <div>${data.Genre.split(",").join("<div></div>")}</div>
+          </div>
+          </div>
+          <h3>Plot:</h3>
+          <p>${data.Plot}</p>
+          <h3>Cast:</h3>
+          <p>${data.Actors}</p>
+          `;
+        }
+        // if movie dosent exist
+        else{
+          result.innerHTML=`<h3 class='msg'> ${data.Error}</h3>`;
+        }
+      }).catch(()=>{
+        result.innerHTML==`<h3 class='msg'>Error Occured</h3>`
+      });
+  }
+};
+searchBtn.addEventListener("click",getMovie);
+window.addEventListener("load", getMovie);
